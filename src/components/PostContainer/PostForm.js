5d31dc5a75ce634e89f1512f113ac9d4e78a4e9d
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-10 19:00:38 -0400

    Added createPost method to PostForm, calls in addPost from action creators and passes in postInfo.

--- src/components/PostContainer/PostForm.js
+++ src/components/PostContainer/PostForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addPost } from "../../actions";
 
 class PostForm extends Component {
   state = {
@@ -16,19 +18,43 @@ class PostForm extends Component {
     }));
   };
 
+  changeHandler = e => {
+    e.persist();
+    this.setState(prevState => ({
+      ...prevState,
+      post: {
+        ...prevState.post,
+        [e.target.name]: e.target.value
+      }
+    }));
+    console.log(this.state);
+  };
+
+  createPost = (e, postInfo) => {
+    e.preventDefault();
+    this.props.addPost(postInfo);
+  };
+
   render() {
     const { open, post } = this.state;
     return (
       <div className="postform">
-        <form>
+        <form onSubmit={e => this.createPost(e, this.state.post)}>
           {open ? (
             <>
               <textarea
                 className="addpost area"
                 placeholder="What's on your mind?"
+                onChange={this.changeHandler}
                 value={post.description}
+                name="description"
               />
-              <input placeholder="Image URL here" value={post.imageUrl} />
+              <input
+                placeholder="Image URL here"
+                onChange={this.changeHandler}
+                value={post.imageUrl}
+                name="imageUrl"
+              />
             </>
           ) : (
             <input
@@ -43,4 +69,7 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+export default connect(
+  null,
+  { addPost }
+)(PostForm);
