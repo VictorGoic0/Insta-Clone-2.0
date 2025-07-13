f49f3a5e5b83364f534e944f2760a62e3a804aea
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-13 16:05:25 -0400

    Consolidate styels

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import "./components/CSS/App.css";
-import { Route } from "react-router-dom";
-import PostsFeed from "./components/PostContainer/PostsFeed";
-import PostPage from "./components/PostContainer/PostPage";
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
-import PrivateRoute from "./components/Authentication/PrivateRoute";
+// import { Route } from "react-router-dom";
+// import PostsFeed from "./components/PostContainer/PostsFeed";
+// import PostPage from "./components/PostContainer/PostPage";
+// import Login from "./components/Login/Login";
+// import Signup from "./components/Signup/Signup";
+// import PrivateRoute from "./components/Authentication/PrivateRoute";
 import { createRoot } from "react-dom/client";
-// import { RouterProvider, createRouter } from "@tanstack/react-router";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { routeTree } from "./routeTree.gen";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
+
 
 const App = () => {
   return (
     <>
-      <PrivateRoute exact path="/" component={PostsFeed} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+      {/* <PrivateRoute exact path="/" component={PostsFeed} />
       <PrivateRoute path="/posts/:id" component={PostPage} />
       <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
+      <Route path="/signup" component={Signup} /> */}
     </>
   );
 };
