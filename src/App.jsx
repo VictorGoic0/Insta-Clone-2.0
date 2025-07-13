b285240e03bd2ffbc23e27eac07652e7fbbbf00f
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-13 16:19:30 -0400

    Lets start from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import "./components/CSS/App.css";
-// import { Route } from "react-router-dom";
-// import PostsFeed from "./components/PostContainer/PostsFeed";
-// import PostPage from "./components/PostContainer/PostPage";
-// import Login from "./components/Login/Login";
-// import Signup from "./components/Signup/Signup";
-// import PrivateRoute from "./components/Authentication/PrivateRoute";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -20,10 +14,6 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
-      {/* <PrivateRoute exact path="/" component={PostsFeed} />
-      <PrivateRoute path="/posts/:id" component={PostPage} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} /> */}
     </>
   );
 };
