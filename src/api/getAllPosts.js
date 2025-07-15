f9cceb6a869b686de3e161bdfc9d65404cdb0788
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-15 11:56:25 -0400

    All requests done

--- src/api/getAllPosts.js
+++ src/api/getAllPosts.js
@@ -1,5 +1,10 @@
-export default async function getPastOrders(page) {
-  const response = await fetch(`/api/past-orders?page=${page}`);
-  const data = await response.json();
-  return data;
+export default async function getAllPosts() {
+  const endpoint = process.env.REACT_APP_BACKENDPOINT;
+
+  try {
+    const response = await fetch(`${endpoint}/api/posts`);
+    return response.json();
+  } catch (error) {
+    throw new Error(`${error.message}. ${error.response.data.message}`);
+  }
 }
--- src/api/getPostById.js
+++ src/api/getPostById.js
@@ -1,5 +1,10 @@
-export default async function getPastOrder(order) {
-  const response = await fetch(`/api/past-order/${order}`);
-  const data = await response.json();
-  return data;
+export default async function getPostById(postId) {
+  const endpoint = process.env.REACT_APP_BACKENDPOINT;
+
+  try {
+    const response = await fetch(`${endpoint}/api/posts/${postId}`);
+    return response.json();
+  } catch (error) {
+    throw new Error(`${error.message}. ${error.response.data.message}`);
+  }
 }
