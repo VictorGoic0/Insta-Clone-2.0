413206cf6d0d01f0109585b1f8276f9489bea5dd
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-15 13:00:35 -0400

    Login and Signup routes

---
+++ src/routes/home.lazy.jsx
@@ -0,0 +1,10 @@
+// import { createLazyFileRoute } from '@tanstack/react-router'
+// import { useMutation } from '@tanstack/react-query'
+
+// export const Route = createLazyFileRoute('/login')({
+//   component: HomeRoute,
+// })
+
+// export default function HomeRoute () {
+
+// }
---
+++ src/routes/signup.lazy.jsx
@@ -0,0 +1,52 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+import { useMutation } from '@tanstack/react-query'
+import signup from "../api/signup"
+
+export const Route = createLazyFileRoute('/signup')({
+  component: SignUpRoute,
+})
+
+export default function SignUpRoute () {
+    const mutation = useMutation({
+    mutationFn: function(e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const userInfo = {
+        username: formData.get("username"),
+        password: formData.get("password")
+      }
+      return signup(userInfo)
+    }
+  })
+
+  const switchLogin = () => {
+    // route to login
+    // this.props.history.push("/login");
+  }
+
+  return (
+      <div className="login">
+        {/* <img src="/Images/iglogo.png" alt="Instagram" /> */}
+        <form onSubmit={mutation.mutate}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+          <button>Sign Up</button>
+        </form>
+        <h3>
+          Already Have An Account?{" "}
+          <span onClick={switchLogin}>Sign In</span>
+        </h3>
+      </div>
+    );
+
+}
