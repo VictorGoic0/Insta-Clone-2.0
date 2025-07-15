4c526914a51213d6fa5b24addba27bac334ab707
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-15 13:18:05 -0400

    Hello world

--- src/routes/home.lazy.jsx
+++ src/routes/home.lazy.jsx
@@ -1,10 +1,12 @@
-// import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router'
 // import { useMutation } from '@tanstack/react-query'
 
-// export const Route = createLazyFileRoute('/login')({
-//   component: HomeRoute,
-// })
+export const Route = createLazyFileRoute('/home')({
+  component: HomeRoute,
+})
 
-// export default function HomeRoute () {
-
-// }
+export default function HomeRoute () {
+  return (
+    <p>Hello World!</p>
+  )
+}
