eef7976638d3d3c4cc73623b2c40c8d61e4661eb
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-15 13:06:31 -0400

    Add route nav

--- src/routes/login.lazy.jsx
+++ src/routes/login.lazy.jsx
@@ -1,51 +1,53 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { useMutation } from '@tanstack/react-query'
 import login from "../api/login"
+import { useNavigate } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/login')({
   component: LoginRoute,
 })
 
 export default function LoginRoute () {
-    const mutation = useMutation({
-    mutationFn: function(e) {
-      e.preventDefault();
-      const formData = new FormData(e.target);
-      const userInfo = {
-        username: formData.get("username"),
-        password: formData.get("password")
-      }
-      return login(userInfo)
-    }
-  })
+  const navigate = useNavigate();
+
+	const mutation = useMutation({
+		mutationFn: function(e) {
+			e.preventDefault();
+			const formData = new FormData(e.target);
+			const userInfo = {
+				username: formData.get("username"),
+				password: formData.get("password")
+			}
+			return login(userInfo)
+		}
+	})
 
   const switchLogin = () => {
-    // route to signup
-    // this.props.history.push("/signup");
+		navigate({ to: "/signup" });
   }
 
   return (
-      <div className="login">
-        <img src="/Images/iglogo.png" alt="Instagram logo" />
-        <form onSubmit={mutation.mutate}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-          <button>Sign In</button>
-        </form>
-        <h3>
-          Don't Have An Account? <span onClick={switchLogin}>Sign Up</span>
-        </h3>
-      </div>
-    );
+	  <div className="login">
+		<img src="/Images/iglogo.png" alt="Instagram logo" />
+		<form onSubmit={mutation.mutate}>
+		  <input
+			type="text"
+			name="username"
+			placeholder="Username"
+			required
+		  />
+		  <input
+			type="password"
+			name="password"
+			placeholder="Password"
+			required
+		  />
+		  <button>Sign In</button>
+		</form>
+		<h3>
+		  Don't Have An Account? <span onClick={switchLogin}>Sign Up</span>
+		</h3>
+	  </div>
+	);
 
 }
