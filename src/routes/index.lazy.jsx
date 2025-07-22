4df7c4e14792f716dcc7ecba46467502348edd78
Victor Goico <vgoico000@citymail.cuny.edu>
2025-07-22 13:49:44 -0400

    PostsFeed scaffold

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -1,14 +1,23 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import { ProtectedRoute } from '../ProtectedRoute'
+import SearchBar from '../SearchBar'
+import PostContainer from '../PostContainer'
+import { useState } from 'react'
 
 export const Route = createLazyFileRoute('/')({
-  component: HomeRoute,
+  component: PostsFeed,
 })
 
-export default function HomeRoute () {
+export default function PostsFeed () {
+  // TODO: add search input state here
+  const [searchInput, setSearchInput] = useState("")
+
+  
+  // TODO: add GET posts state here
   return (
     <ProtectedRoute>
-      <p>Hello World!</p>
+      <SearchBar />
+      <PostContainer searchInput={searchInput} />
     </ProtectedRoute>
   )
 }
